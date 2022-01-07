c5689d7157ab56e97779deaef4a7d63f1b976e62
Laura Demougeot <laurademougeot26@gmail.com>
2022-01-07 14:22:54 +0100

    header + footer en cour

---
+++ src/components/Footer.vue
@@ -0,0 +1,236 @@
+<template>
+  <footer>
+    <hr />
+    <div class="c-footer__container">
+      <div class="row -center">
+        <div class="col-4">
+          <svg
+            class="logoFooter"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 178 74.47"
+          >
+            <g id="Calque_2" data-name="Calque 2">
+              <g id="Calque_1-2" data-name="Calque 1">
+                <path
+                  d="M122.1,36.75a7,7,0,1,1,7,7A7,7,0,0,1,122.1,36.75Z"
+                  style="fill: #fbf5f3; fill-rule: evenodd"
+                />
+                <path
+                  d="M134.83,68.77a5.72,5.72,0,0,1-10.27,3.44l-.19-.26L111.28,54.26V68.4c0,.12,0,.24,0,.36a5.73,5.73,0,0,1-11.45.07h0V37.33h0c0-.15,0-.3,0-.45a5.72,5.72,0,0,1,10.53-3.08v0l23.4,31.64h0A5.65,5.65,0,0,1,134.83,68.77Z"
+                  style="fill: #fbf5f3; fill-rule: evenodd"
+                />
+                <path
+                  d="M18.69,4.5h0s0,0,0,.06Z"
+                  style="fill: #fbf5f3; fill-rule: evenodd"
+                />
+                <path
+                  d="M32.1,8.55,32,8.82a2.29,2.29,0,0,0,.07-.28Z"
+                  style="fill: #fbf5f3; fill-rule: evenodd"
+                />
+                <path
+                  d="M32.21,7.12a8.33,8.33,0,0,1-.13,1.42c0,.09,0,.18-.07.28s0,0,0,.08l-.23.74L13.6,69.4h0A7,7,0,0,1,7,74.06,7.14,7.14,0,0,1,.57,64.13L18.39,5.49a0,0,0,0,1,0,0l.27-.88v0s0,0,0-.06A7,7,0,0,1,32.21,7.12Z"
+                  style="fill: #fbf5f3; fill-rule: evenodd"
+                />
+                <path
+                  d="M18.4,5.46a6.87,6.87,0,0,1,.27-.88Z"
+                  style="fill: #fbf5f3; fill-rule: evenodd"
+                />
+                <path
+                  d="M75.08,62.6h0l-30.6-.06h0a10.19,10.19,0,0,1,0-20.38h0l30.6.06h0a10.19,10.19,0,0,1,0,20.38ZM90.4,37.21a21.64,21.64,0,0,0-15.33-6.33l-30.6-.06a21.59,21.59,0,1,0,0,43.18l30.6.06A21.57,21.57,0,0,0,90.4,37.21Z"
+                  style="fill: #fbf5f3; fill-rule: evenodd"
+                />
+                <path
+                  d="M122,45.8l0,.05c-.09.21,0,.11,0,0S122.05,45.79,122,45.8Z"
+                  style="fill: #fbf5f3; fill-rule: evenodd"
+                />
+                <path
+                  d="M149.46,58a5.87,5.87,0,0,1-.5-1.22q-.19-.69-.33-1.44c-.09-.5-.15-1-.19-1.47s-.06-.92-.06-1.31a12.4,12.4,0,0,1,.81-4.6,10.37,10.37,0,0,1,2.22-3.5,9.69,9.69,0,0,1,3.32-2.24,10.55,10.55,0,0,1,4.08-.79,14.26,14.26,0,0,1,1.49.08,6,6,0,0,1,1.45.35ZM178,60.88a5.28,5.28,0,0,0-5.24-5.33,5.19,5.19,0,0,0-4.3,2.3c.06,0,0,.06,0,.17s-.29.46-.07,0l.09-.13a1,1,0,0,0-.21.2,34.64,34.64,0,0,1-3.78,4.05,10.51,10.51,0,0,1-5.66,1.63q-.62,0-1.23-.06a6.06,6.06,0,0,1-1.24-.25l16.92-20.18a5.09,5.09,0,0,0-.5-7.12l0,0a21.57,21.57,0,0,0-4.16-2.81,21.24,21.24,0,0,0-4.7-1.76,20.81,20.81,0,0,0-5.06-.62,21.62,21.62,0,0,0-8.21,1.57,20.78,20.78,0,0,0-6.72,4.4,20.45,20.45,0,0,0-4.54,6.85,23,23,0,0,0-1.66,8.87,22.24,22.24,0,0,0,1.66,8.71,21,21,0,0,0,4.54,6.83,20.64,20.64,0,0,0,6.72,4.47,20.91,20.91,0,0,0,8.21,1.61,21.21,21.21,0,0,0,5.05-.59,20.82,20.82,0,0,0,4.67-1.74,21.68,21.68,0,0,0,4.14-2.8,20.2,20.2,0,0,0,4.22-4.91h0A5.27,5.27,0,0,0,178,60.88Z"
+                  style="fill: #fbf5f3; fill-rule: evenodd"
+                />
+                <path
+                  d="M122,45.87c0,.09-.12.19,0,0l0-.05S122,45.83,122,45.87Z"
+                  style="fill: #fbf5f3; fill-rule: evenodd"
+                />
+              </g>
+            </g>
+          </svg>
+          <div class="c-footer_containerSocialNetwork">
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fab"
+              data-icon="instagram"
+              class="svg-inline--fa fa-instagram fa-w-14"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+            >
+              <path
+                fill="currentColor"
+                d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
+              ></path>
+            </svg>
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fab"
+              data-icon="tiktok"
+              class="svg-inline--fa fa-tiktok fa-w-14"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+            >
+              <path
+                fill="currentColor"
+                d="M448,209.91a210.06,210.06,0,0,1-122.77-39.25V349.38A162.55,162.55,0,1,1,185,188.31V278.2a74.62,74.62,0,1,0,52.23,71.18V0l88,0a121.18,121.18,0,0,0,1.86,22.17h0A122.18,122.18,0,0,0,381,102.39a121.43,121.43,0,0,0,67,20.14Z"
+              ></path>
+            </svg>
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fab"
+              data-icon="facebook-f"
+              class="svg-inline--fa fa-facebook-f fa-w-10"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 320 512"
+            >
+              <path
+                fill="currentColor"
+                d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"
+              ></path>
+            </svg>
+          </div>
+        </div>
+        <div class="col-2">
+          <h3 class="fontFooter -footerTitle">Aide</h3>
+          <ul class="fontFooter">
+            <li><a href="">texte</a></li>
+            <li><a href="">texte</a></li>
+            <li><a href="">texte</a></li>
+            <li><a href="">texte</a></li>
+            <li><a href="">texte</a></li>
+          </ul>
+        </div>
+        <div class="col-2">
+          <h3 class="fontFooter -footerTitle">a propos de</h3>
+          <ul class="fontFooter">
+            <li><a href="">texte</a></li>
+            <li><a href="">texte</a></li>
+            <li><a href="">texte</a></li>
+            <li><a href="">texte</a></li>
+            <li><a href="">texte</a></li>
+          </ul>
+        </div>
+        <div class="col-4">
+          <h3 class="fontFooter -footerTitle">personnalisés</h3>
+          <p class="fontFooter">
+            Choisis ton modèle préféré et crées-en une paire unique avec notre
+            outli de personnalisation.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="c-footer__containerCGU">
+      <div>
+        <p class="fontFooter -fontCGU">
+          Réalisé dans le cadre d’un projet pédagogique au sein du département
+          MMI Montbéliard
+        </p>
+      </div>
+      <div>
+        <p class="fontFooter -fontCGU">
+          <a href="">Conditions d'utilisation</a> |
+          <a href="">Conditions générales de vente</a> |
+          <a href="">Mentions légales</a>
+        </p>
+        <p class="fontFooter -fontCGU">
+          <a href="">Politique en matières de confidentitalités de cookies</a> |
+          <a href="">Paramètres des cookies</a>
+        </p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+</script>
+
+
+<style lang="scss">
+footer {
+  h3 {
+    margin-top: 0;
+  }
+  ul {
+    padding: 0;
+  }
+  li {
+    width: 100%;
+  }
+  a {
+    color: $white;
+    text-decoration: none;
+  }
+  a:hover {
+    text-decoration: underline $primaryColor;
+  }
+  hr {
+    max-width: 800px;
+    margin: auto;
+    border-color: $primaryColor;
+    margin-top: 100px;
+    margin-bottom: 50px;
+  }
+}
+.logoFooter {
+  height: 100px;
+  width: auto;
+  display: block;
+  margin: auto;
+  margin-bottom: 70px;
+}
+.c-footer__container {
+  max-width: 1100px;
+  margin: auto;
+  .row {
+    div:first-child {
+      img {
+        width: 200px;
+        display: block;
+        margin: auto;
+      }
+    }
+  }
+}
+
+.c-footer_containerSocialNetwork {
+  display: flex;
+  justify-content: center;
+  color: $white;
+  svg {
+    height: 25px;
+    width: auto;
+    transition: 0.25s ease-in-out;
+    cursor: pointer;
+
+    &:nth-child(n + 2) {
+      margin-left: 30px;
+    }
+    &:hover {
+      color: $primaryColor;
+    }
+  }
+}
+
+.c-footer__containerCGU {
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: flex-end;
+  background-color: $black;
+  p {
+    margin: 0;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,17 +3,23 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <p>cora c super</p>
+   
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue'
+import Footer from '../components/Footer.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    Header,
+    Footer
   }
 }
 </script>
