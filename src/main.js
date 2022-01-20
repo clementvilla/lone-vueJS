8c32196899a09031decab07e2097882c00735f85
Biron <pierrebiron.lsw@gmail.com>
2022-01-20 08:17:37 +0100

    avancement

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Particles from 'particles.vue';
+
 
 Vue.config.productionTip = false
+
+Vue.use(Particles);
 
 new Vue({
   router,
