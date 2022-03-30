00fcdcf05fe261d6149beca2859b6b546fbbaee7
Villa <clement.villa@edu.univ-fcomte.fr>
2022-03-30 21:55:52 +0200

    derniere modif header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -45,7 +45,7 @@
               <div class="c-header__innerContainer">
                 <div class="e-header__log">
                   <p>Bonjour {{user.displayName}}</p>
-                  <router-link to="/compte" class="fontLink"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" ><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg></router-link>
+                  <router-link to="/login" class="fontLink"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" ><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg></router-link>
                   
                   <router-link to="/compte" class="fontLink"><svg v-if="user.displayName != null" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                   </svg></router-link>
